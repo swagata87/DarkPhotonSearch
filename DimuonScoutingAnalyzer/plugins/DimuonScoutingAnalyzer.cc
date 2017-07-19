cf6f8cbf11f2fdd8d209d27edf91977ecdf5f341
Swagata Mukherjee <s.mukherjee@cern.ch>
2017-07-19 22:06:29 +0200

    small change

--- DimuonScoutingAnalyzer/plugins/DimuonScoutingAnalyzer.cc
+++ DimuonScoutingAnalyzer/plugins/DimuonScoutingAnalyzer.cc
@@ -122,7 +122,7 @@ DimuonScoutingAnalyzer::DimuonScoutingAnalyzer(const edm::ParameterSet& iConfig)
   
   TFileDirectory histoDir = fs->mkdir("histoDir");
   
-  Float_t bins[] = {0.,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,25,30,40,50,60,70,80,90,100};
+  Float_t bins[] = {0.,0.01,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,25,30,40,50,60,70,80,90,100};
   Int_t  binnum = sizeof(bins)/sizeof(Float_t) - 1; // or just write the number
     
   h1_dimuonMass                = histoDir.make<TH1F>("dimuMass", "Mmumu", binnum, bins);
